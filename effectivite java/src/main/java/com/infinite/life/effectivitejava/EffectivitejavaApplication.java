1fd5c2ff3d321c6275f41e6600f1ba3c54037d63
cuiwx <cuiwx123>
2019-11-27 12:54:49 +0800

    STUDY - 2 effectivite java add

---
+++ effectivite java/src/main/java/com/infinite/life/effectivitejava/EffectivitejavaApplication.java
@@ -0,0 +1,13 @@
+package com.infinite.life.effectivitejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectivitejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectivitejavaApplication.class, args);
+    }
+
+}
---
+++ effectivite java/src/test/java/com/infinite/life/effectivitejava/EffectivitejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinite.life.effectivitejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectivitejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
